b2c0c8ffb37b5b8bc9674cd604b77da2977a1cea
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-10 10:22:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const horas = document.getElementById('horas');
+
+const minutos = document.getElementById('minutos');
+
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(functiontime
