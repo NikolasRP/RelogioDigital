b7cccb716fc7df8961d3ed56faa6fb024f456a7c
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-11 08:09:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ const minutos = document.getElementById('minutos');
 
 const segundos = document.getElementById('segundos');
 
-const relogio = setInterval(functiontime
+const relogio = setInterval(functiontime(){
+                            let dateToday = newDate();
+  let hr = dateToday.getHours();
+  let min = dateToday.getMinutes();
+  let s = dateToday.getSeconds();
+
+  horas.textcontent = hr;
+  minutos.textcontent = min;
+  segundos.textcontent = s;
+
+  if (hr < 10) hr = '0' + hr;
+  if (hr < 10) hr = '0' + hr;
+  if (hr < 10) hr = '0' + hr;
+
+})
